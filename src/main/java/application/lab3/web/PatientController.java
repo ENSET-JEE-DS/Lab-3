f3923e8fe05fdd5b9d3d246d2956e3e626df685c
Yahya Ghallali <Yahyaghallali88@gmail.com>
2025-03-18 03:34:43 +0000

    Part 1: Done

--- src/main/java/application/lab3/web/PatientController.java
+++ src/main/java/application/lab3/web/PatientController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -38,5 +39,11 @@ public class PatientController {
     public String deletePatient(Long id, @RequestParam(name = "p") int page, String keyword) {
         patientRepository.deleteById(id);
         return "redirect:/index?p=" + page + "&keyword=" + keyword;
+    }
+
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
     }
 }
